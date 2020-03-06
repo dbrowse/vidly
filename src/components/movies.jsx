95627779544ea652a19f9b35dc29d685da3c6638
dgeorgiev <dgeorgiev_job@yahoo.com>
2020-03-06 21:51:45 +0200

    Delete button logic

---
+++ src/components/movies.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+
+class Movies extends Component {
+	state = {
+		movies: getMovies()
+	};
+
+	handleDelete = movie => {
+		//new state for the object
+		const movies = this.state.movies.filter(m => m._id !== movie._id);
+		this.setState({ movies });
+	};
+	render() {
+		return (
+			<div>
+				<span>Showing 9 movies in database </span>
+				<table className='table'>
+					<thead>
+						<tr>
+							<th scope='col'>Title</th>
+							<th scope='col'>Genre</th>
+							<th scope='col'>Stock</th>
+							<th scope='col'>Rate</th>
+						</tr>
+					</thead>
+					<tbody>
+						{this.state.movies.map(movie => (
+							<tr key={movie._id}>
+								<td>{movie.title}</td>
+								<td>{movie.genre.name}</td>
+								<td>{movie.numberInStock}</td>
+								<td>{movie.dailyRentalRate}</td>
+								<td>
+									<button
+										onClick={() => this.handleDelete(movie)}
+										type='button'
+										className='btn btn-danger btn-sm'
+									>
+										Delete
+									</button>
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</div>
+		);
+	}
+}
+
+export default Movies;
